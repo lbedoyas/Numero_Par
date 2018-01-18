d08e8c25c150eda8727be26f158da01b20b1b222
LB <lucho.bedoya1@hotmail.com>
2018-01-18 18:35:48 -0500

    Numero_Par
    Un programa que permite saber si un numero es par o no

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Ingrese el numero: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int e;
+            double d;
+            e = n / 2;
+            d = n / 2;
+            if(d - e == 0)
+            {
+                Console.WriteLine("El numero: {0}  es par",n);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("El numero:  {0}  es impar",n);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
